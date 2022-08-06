1fce6f3ae77aef7dacda9001d9472e6bc721b428
kamal sharma <kksharma6161@gmail.com>
2022-08-06 12:07:16 +0530

    default route changed to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 const routes: Routes = [
   {
     path: "",
-    redirectTo: "landing",
+    redirectTo: "signup",
     pathMatch: "full"
   },
   {
